7cca5eeb4bc1c6713695d8d552bb24bae99293cd
Reza Ramezanpour <12220097+RezaRamezanpour@users.noreply.github.com>
2023-02-15 17:29:26 +0330

    Update persian-slug-generator.php

--- src/persian-slug-generator.php
+++ src/persian-slug-generator.php
@@ -1,3 +1,9 @@
+function persianToEnglishDigits($string)
+{
+	$persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+	$english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	return str_replace($persian, $english, $string);
+}
 
 /**
  * Generates URL Slug
@@ -6,32 +12,37 @@
  *                                     length will be removed from the slug
  * @param  string  $separator         separator char
  * @return string                     the generated slug
+ * 
+ * @see https://github.com/RezaRamezanpour/persian-slug-generator
  */
 function generateSlug($string, $ignoreShortWords = false, $separator = '-')
 {
-    $allowed = implode('',[
-        '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰',
-        'آ', 'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د',
-        'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ',
-        'ع', 'ق', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و',
-        'ه', 'ی', 'ئ',
-        'a-z', '0-9',
-	]);
-    $delimiters = implode('',[' ', '|', '_', '(', ')', ',', '،']);
-    $words  = preg_split('/[' . $delimiters . ']/u', $string);
-    $string = null;
-    if ($ignoreShortWords) {
-        foreach ($words as $k => $word) {
-            if (mb_strlen($word) < 3) unset($words[$k]);
-        }
-    }
+	if (!$string) return;
 
-    foreach ($words as $word) {
-        $word = strtolower($word);
-        $word = mb_ereg_replace('([^' . $allowed . '])+', '', $word);
-        if (mb_strlen($word) < 1)  continue;
-        $string .= $word .= $separator;
-    }
-    
-    return trim($string, $separator);
+	$allowed = implode('', [
+		'آ', 'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د',
+		'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ',
+		'ع', 'ق', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و',
+		'ه', 'ی', 'ئ',
+		'a-z', '0-9',
+	]);
+
+	$string = persianToEnglishDigits(urldecode($string));
+	$delimiters = implode('', [' ', '\-', '|', '_', '(', ')', ',', '،']);
+	$words  = preg_split('/[' . $delimiters . ']/u', $string);
+	$string = null;
+	if ($ignoreShortWords) {
+		foreach ($words as $k => $word) {
+			if (mb_strlen($word) < 3) unset($words[$k]);
+		}
+	}
+
+	foreach ($words as $word) {
+		$word = strtolower($word);
+		$word = mb_ereg_replace('([^' . $allowed . '])+', '', $word);
+		if (mb_strlen($word) < 1)  continue;
+		$string .= $word .= $separator;
+	}
+
+	return trim($string, $separator);
 }
